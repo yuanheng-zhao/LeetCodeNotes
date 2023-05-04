f2da01bd1b7829090284480b0f6e1952d645c403
ocd_with_naming <jonathan.zhaoyh@gmail.com>
2023-05-04 10:51:08 -0700

    refactor lc79

--- bfs_dfs/79.Word_Search.py
+++ bfs_dfs/79.Word_Search.py
@@ -4,28 +4,34 @@ class Solution:
 
         m = len(board)
         n = len(board[0])
+        self.word = word  # 小偷一手
+        self.board = board
         
-        def dfs(i: int, j: int, idx:int, visiting: List[List[bool]]) -> bool:
-            if idx == len(word):
-                return True
-            if i < 0 or j < 0 or i >= m or j >= n:
-                return False
-            if visiting[i][j] or board[i][j] != word[idx]:
-                return False
-            
-            res = False
-            visiting[i][j] = True
-            res = dfs(i-1, j, idx+1, visiting) or dfs(i+1, j, idx+1, visiting) or dfs(i, j-1, idx+1, visiting) or dfs(i, j+1, idx+1, visiting)
-            visiting[i][j] = False
-            return res
-
         # visiting instead of visited
         # visiting indicating specific cells have been visited, or have been visiting during the current search on current
         # chain of characters, and so that we should not use it twice
         visiting_mat = [[False for _ in range(n)] for _ in range(m)]
         for i in range(m):
             for j in range(n):
-                if dfs(i, j, 0, visiting_mat):
+                if self.dfs(visiting_mat, i, j, 0):
                     return True
-
+        
         return False
+
+
+    def dfs(self, visiting: List[List[bool]], i: int, j: int, idx: int) -> bool:
+        if idx == len(self.word):
+            return True
+        if i < 0 or i >= len(visiting) or j < 0 or j >= len(visiting[0]):
+            return False
+        if visiting[i][j] or self.board[i][j] != self.word[idx]:
+            return False
+        
+        visiting[i][j] = True
+        idx += 1
+        res = (self.dfs(visiting, i-1, j, idx) 
+               or self.dfs(visiting, i+1, j, idx)
+               or self.dfs(visiting, i, j-1, idx)
+               or self.dfs(visiting, i, j+1, idx))
+        visiting[i][j] = False
+        return res
